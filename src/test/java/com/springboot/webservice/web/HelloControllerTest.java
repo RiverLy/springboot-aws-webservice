ff5ab09a2228efee621f821c2c715f601cc86e63
Garam <emteofh0313@naver.com>
2021-07-20 22:32:42 +0900

    네이버 로그인 기능 추가

--- src/test/java/com/springboot/webservice/web/HelloControllerTest.java
+++ src/test/java/com/springboot/webservice/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.springboot.webservice.web;
 
+import com.springboot.webservice.config.auth.SecurityConfig;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
@@ -14,7 +18,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 
 public class HelloControllerTest {
 
@@ -22,6 +28,7 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void hello() throws Exception{
         String hello = "hello";
 
@@ -32,6 +39,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto() throws  Exception{
         String name = "hello";
         int amount = 1000;
